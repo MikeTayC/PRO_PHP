a0c6792d4481f564ed314e1222e8c17c983216fa
MikeTayC <miketayc@gmail.com>
2015-03-04 17:17:38 -0500

    calendar
    its a fucking calendar

---
+++ public/admin.php
@@ -0,0 +1,3 @@
+/**
+ *To create or modify new events, the form displayed on this page is used.
+ */
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php 
+/**
+ * This is the main file, which displays the month in calendar format with
+ * event titles displayed in the box of the day on which they occur. 
+ */
+ 
